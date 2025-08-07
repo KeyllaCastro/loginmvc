e115f2262b93bc068a9f42e5e5bd1805e49168c8
android <rayane.tahara@etec.sp.gov.br>
2025-08-06 22:08:09 -0300

    Atualizacao dos pacotes e Classes MVC

--- app/src/main/java/android/fabio/loginmvc/datamodel/UsuarioDataModel.java
+++ app/src/main/java/android/fabio/loginmvc/datamodel/UsuarioDataModel.java
@@ -1,4 +1,29 @@
 package android.fabio.loginmvc.datamodel;
 
+import android.util.Log;
+
 public class UsuarioDataModel {
+    //atributo com nome da tabela
+    public static final String TABELA = "usuario";
+    //atributos 1 para 1
+    public static final String ID = "id";
+    public static final String NOME = "nome";
+    public static final String EMAIL = "emial";
+    public static final String SENHA = "semha";
+
+    public static  String queryCriarTabela = "";
+
+    public static String criarTabela(){
+        queryCriarTabela += "CREATE TABLE " + TABELA  + "(";
+        queryCriarTabela += ID + " int primary key autoincrement";
+        queryCriarTabela += NOME + " text, ";
+        queryCriarTabela += EMAIL + " text, ";
+        queryCriarTabela += SENHA + " text, ";
+        queryCriarTabela += ")";
+
+            Log.i("tabela", "criarTabela: " + queryCriarTabela);
+        return  queryCriarTabela;
+
+
+    }
 }
--- app/src/main/java/android/fabio/loginmvc/datasource/AppDataBase.java
+++ app/src/main/java/android/fabio/loginmvc/datasource/AppDataBase.java
@@ -1,4 +1,30 @@
 package android.fabio.loginmvc.datasource;
 
-public class AppDataBase {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.fabio.loginmvc.datamodel.UsuarioDataModel;
+
+import androidx.annotation.Nullable;
+
+public class AppDataBase extends SQLiteOpenHelper {
+    SQLiteDatabase db;
+    public static final String DB_NAME = "mvc.sqlite";
+    public static int  version = 1;
+    public AppDataBase( Context context) {
+        super(context, DB_NAME, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db = getWritableDatabase();
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(UsuarioDataModel.criarTabela());
+
+    }
 }
